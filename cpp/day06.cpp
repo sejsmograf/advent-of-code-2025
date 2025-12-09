36b9126b961cccec2718616854d28bcfc1c07bae
tomasz-mroczko <tomasz.mroczko@vodafone.com>
2025-12-09 18:22:57 +0100

    day 08

--- cpp/day06.cpp
+++ cpp/day06.cpp
@@ -1,46 +1,38 @@
 #include <cstdint>
-#include <functional>
-#include <iostream>
 #include <fstream>
-#include <numeric>
+#include <iostream>
 #include <print>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 using ul = uint64_t;
-using Column = std::vector<ul>;
+using Row = std::vector<ul>;
 using Operators = std::vector<char>;
-using Equation = std::pair<std::vector<Column>, Operators>;
+using Equation = std::pair<std::vector<Row>, Operators>;
 
 Equation readInput() {
-
   Equation equation;
 
   std::ifstream is("../input/day06.txt");
   std::string line;
-
-  std::getline(is, line);
   std::stringstream ss(line);
-  size_t numberIndex;
-  ul number;
-
-  while (ss >> number) {
-    equation.first.push_back({number});
-  }
 
   while (std::getline(is, line)) {
+    Row row;
+    ul number;
     ss = std::stringstream(line);
+
     if (line[0] == '*' || line[0] == '+') {
       break;
     }
 
-    size_t index = 0;
-
     while (ss >> number) {
-      equation.first[index].push_back(number);
-      index++;
+      row.push_back(number);
     }
+
+    equation.first.push_back(row);
   }
 
   char op;
@@ -48,37 +40,77 @@ Equation readInput() {
     equation.second.push_back(op);
   }
 
-  // for (int i = 0; i < equation.first[0].size(); i++) {
-  //   std::println("columns {}", equation.first[i].size());
-  // }
-
   return equation;
 };
+
+std::vector<std::string> transposeInput() {
+  std::ifstream is("../input/day06.txt");
+  std::vector<std::string> lines;
+  std::string line;
+
+  while (std::getline(is, line)) {
+    lines.push_back(line);
+  }
+
+  lines.pop_back();
+
+  std::vector<std::string> transposedLines(lines[0].size(), "");
+
+  for (size_t row = 0; row < lines.size(); row++) {
+    for (size_t col = 0; col < lines[0].size(); col++) {
+      transposedLines[col] += lines[row][col];
+    }
+  }
+
+  return transposedLines;
+}
 
 ul silver(const Equation &input) {
   ul total = 0;
 
-  for (size_t i = 0; i < input.first.size(); i++) {
-    char op = input.second[i];
+  for (size_t i = 0; i < input.first[0].size(); i++) {
+    ul lineTotal = input.second[i] == '*' ? 1 : 0;
 
-    if (op == '*') {
-      total += std::accumulate(input.first[i].begin(), input.first[i].end(), 1,
-                               std::multiplies<ul>());
-    } else if (op == '+') {
-      total += std::accumulate(input.first[i].begin(), input.first[i].end(), 0);
+    for (size_t j = 0; j < input.first.size(); j++) {
+      if (input.second[i] == '*') {
+        lineTotal *= input.first[j][i];
+      } else if (input.second[i] == '+') {
+        lineTotal += input.first[j][i];
+      }
     }
+
+    total += lineTotal;
   }
 
   return total;
 }
 
+ul gold(const std::vector<char> &operators) {
+  auto input = transposeInput();
+  ul total = 0;
+  size_t operatorIndex = 0;
+  ul lineTotal = operators[operatorIndex] == '*' ? 1 : 0;
+
+  for (const auto &s : input) {
+    try {
+      if (operators[operatorIndex] == '*') {
+        lineTotal *= std::stoul(s);
+      } else if (operators[operatorIndex] == '+') {
+        lineTotal += std::stoul(s);
+      }
+    } catch (const std::exception &) {
+      total += lineTotal;
+      operatorIndex++;
+      lineTotal = operators[operatorIndex] == '*' ? 1 : 0;
+    }
+  }
+
+  return total + lineTotal;
+}
+
 int main() {
   auto input = readInput();
 
-  std::println("\nSilver {}", silver(input));
-
-  // printEquation(input);
-  // for (auto dupa : input.first[1]) {
-  //   std::println("{}", dupa);
-  // }
+  std::println("Silver {}", silver(input));
+  std::println("Gold {}", gold(input.second));
 }
---
+++ cpp/day08.cpp
@@ -0,0 +1,173 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <print>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using ul = unsigned long long;
+using Point = std::tuple<ul, ul, ul>;
+using PointPair = std::pair<Point, Point>;
+
+std::vector<Point> readInput() {
+  std::vector<Point> input;
+
+  std::ifstream is("../input/day08.txt");
+  std::string line;
+
+  while (std::getline(is, line)) {
+    std::stringstream ss(line);
+    ul coordX;
+    ul coordY;
+    ul coordZ;
+    char sep;
+
+    ss >> coordX;
+    ss >> sep;
+    ss >> coordY;
+    ss >> sep;
+    ss >> coordZ;
+    input.push_back({coordX, coordY, coordZ});
+  }
+
+  return input;
+};
+
+double euclideanDistance(Point point1, Point point2) {
+    double dx = (double)std::get<0>(point1) - (double)std::get<0>(point2);
+    double dy = (double)std::get<1>(point1) - (double)std::get<1>(point2);
+    double dz = (double)std::get<2>(point1) - (double)std::get<2>(point2);
+
+    return std::sqrt(dx*dx + dy*dy + dz*dz);
+}
+
+std::vector<std::pair<PointPair, double>>
+findClosestPairs(const std::vector<Point> &input) {
+  using Entry = std::pair<PointPair, double>;
+
+  std::set<PointPair> inserted;
+  std::vector<std::pair<PointPair, double>> pairsWithDistance;
+
+  auto cmp = [](Entry a, Entry b) { return a.second < b.second; };
+  std::priority_queue<Entry, std::vector<Entry>, decltype(cmp)> heap;
+
+  for (auto point1 : input) {
+    for (const auto point2 : input) {
+      if (point1 == point2 || inserted.contains({point2, point1}))
+        continue;
+
+      pairsWithDistance.push_back({{point1, point2}, euclideanDistance(point1, point2)});
+      inserted.insert({point1, point2});
+    }
+  }
+
+  std::sort(pairsWithDistance.begin(), pairsWithDistance.end(),
+            [](auto a, auto b) { return a.second < b.second; });
+
+  return pairsWithDistance;
+}
+
+ul silver(const std::vector<std::pair<PointPair, double>>& closestPairs, size_t n) {
+  std::vector<std::set<Point>> circuits;
+
+  for (size_t i = 0; i < n; i++) {
+    auto [point1, point2] = closestPairs[i].first;
+
+    auto containingFirst =
+        std::find_if(circuits.begin(), circuits.end(),
+                     [point1](auto c) { return c.contains(point1); });
+
+    auto containingSecond =
+        std::find_if(circuits.begin(), circuits.end(),
+                     [point2](auto c) { return c.contains(point2); });
+
+    if (containingFirst == circuits.end() &&
+        containingSecond == circuits.end()) {
+      circuits.push_back({point1, point2});
+    } else if (containingFirst == containingSecond) {
+      continue;
+    } else if (containingFirst != circuits.end() &&
+               containingSecond != circuits.end()) {
+      if (containingFirst < containingSecond) {
+        containingFirst->insert(containingSecond->begin(),
+                                containingSecond->end());
+        circuits.erase(containingSecond);
+      } else {
+        containingSecond->insert(containingFirst->begin(),
+                                 containingFirst->end());
+        circuits.erase(containingFirst);
+      }
+    } else if (containingFirst != circuits.end()) {
+      containingFirst->insert(point2);
+    } else if (containingSecond != circuits.end()) {
+      containingSecond->insert(point1);
+    }
+  }
+
+
+  std::sort(circuits.begin(), circuits.end(), [](auto a, auto b) {
+    return a.size() > b.size();
+  });
+
+  return circuits[0].size() * circuits[1].size() * circuits[2].size();
+}
+
+
+ul gold(const std::vector<std::pair<PointPair, double>>& closestPairs, size_t n) {
+  std::vector<std::set<Point>> circuits;
+
+  size_t index = 0;
+  Point point1, point2;
+  while (circuits.size() != 1 || circuits[0].size() != n) {
+    point1 = closestPairs[index].first.first;
+    point2 = closestPairs[index].first.second;
+    double dist = closestPairs[index].second;
+    index++;
+
+
+    auto containingFirst =
+        std::find_if(circuits.begin(), circuits.end(),
+                     [point1](auto c) { return c.contains(point1); });
+
+    auto containingSecond =
+        std::find_if(circuits.begin(), circuits.end(),
+                     [point2](auto c) { return c.contains(point2); });
+
+    if (containingFirst == circuits.end() &&
+        containingSecond == circuits.end()) {
+      circuits.push_back({point1, point2});
+    } else if (containingFirst == containingSecond) {
+      continue;
+    } else if (containingFirst != circuits.end() &&
+               containingSecond != circuits.end()) {
+      if (containingFirst < containingSecond) {
+        containingFirst->insert(containingSecond->begin(),
+                                containingSecond->end());
+        circuits.erase(containingSecond);
+      } else {
+        containingSecond->insert(containingFirst->begin(),
+                                 containingFirst->end());
+        circuits.erase(containingFirst);
+      }
+    } else if (containingFirst != circuits.end()) {
+      containingFirst->insert(point2);
+    } else if (containingSecond != circuits.end()) {
+      containingSecond->insert(point1);
+    }
+  }
+
+
+  return std::get<0>(point1) * std::get<0>(point2);
+}
+
+int main() {
+  auto input = readInput();
+  auto closestPairs = findClosestPairs(input);
+
+  std::println("Silver {}", silver(closestPairs, 10));
+  std::println("Gold {}", gold(closestPairs, input.size()));
+}
